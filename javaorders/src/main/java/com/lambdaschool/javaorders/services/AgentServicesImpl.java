aef31ad5c0a76c29461b35a937123fbfce6b1d8d
Jacob Olness <jacobolness@Jacobs-MacBook-Pro.local>
2021-02-16 15:58:42 -0700

    Agent Controller Done

--- javaorders/src/main/java/com/lambdaschool/javaorders/services/AgentServicesImpl.java
+++ javaorders/src/main/java/com/lambdaschool/javaorders/services/AgentServicesImpl.java
@@ -6,6 +6,7 @@ import com.lambdaschool.javaorders.repositories.AgentsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
 import javax.transaction.Transactional;
 
 @Transactional
@@ -19,5 +20,11 @@ public class AgentServicesImpl implements AgentServices {
     public Agent save(Agent agent)
     {
         return agentrepos.save(agent);
+    }
+
+    @Override
+    public Agent findAgentById(long agentid) {
+        return agentrepos.findById(agentid)
+        .orElseThrow(() -> new EntityNotFoundException("Agent #" + agentid + " not found!"));
     }
 }
