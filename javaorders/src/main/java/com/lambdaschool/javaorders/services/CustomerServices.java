7acf6e2cc43c7a52d8be893eb29d5dcff242268a
Jacob Olness <jacobolness@Jacobs-MacBook-Pro.local>
2021-02-16 14:58:39 -0700

    First two routes are done

--- javaorders/src/main/java/com/lambdaschool/javaorders/services/CustomerServices.java
+++ javaorders/src/main/java/com/lambdaschool/javaorders/services/CustomerServices.java
@@ -2,6 +2,12 @@ package com.lambdaschool.javaorders.services;
 
 import com.lambdaschool.javaorders.models.Customer;
 
+import java.util.List;
+
 public interface CustomerServices {
     Customer save(Customer customer);
+
+    List<Customer> findAllCustomers();
+
+    Customer findCustomerById(long custid);
 }
--- javaorders/src/main/java/com/lambdaschool/javaorders/services/CustomerServicesImpl.java
+++ javaorders/src/main/java/com/lambdaschool/javaorders/services/CustomerServicesImpl.java
@@ -6,7 +6,10 @@ import com.lambdaschool.javaorders.repositories.CustomersRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
 import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.List;
 
 @Transactional
 @Service
@@ -20,4 +23,19 @@ public class CustomerServicesImpl implements CustomerServices {
     {
         return custrepos.save(customer);
     }
+
+    @Override
+    public List<Customer> findAllCustomers() {
+        List<Customer> list = new ArrayList<>();
+        custrepos.findAll().iterator().forEachRemaining(list::add);
+        return list;
+    }
+
+    @Override
+    public Customer findCustomerById(long custid) {
+        return custrepos.findById(custid)
+        .orElseThrow(() -> new EntityNotFoundException("Customer #" + custid + " not found!" ));
+    }
+
+
 }
